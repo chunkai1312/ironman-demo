2b4cb1a60ef8b9966e27b66d0fc1c19e8aa4df5f
Chun-Kai Wang <chunkai1312@gmail.com>
2022-07-03 15:48:17 +0800

    feat(scraper): fetch OTC listed

--- apps/scraper/src/app/scraper/twse-scraper.service.ts
+++ apps/scraper/src/app/scraper/twse-scraper.service.ts
@@ -32,4 +32,29 @@ export class TwseScraperService {
 
     return data;
   }
+
+  async fetchOtcListed() {
+    const url = 'https://isin.twse.com.tw/isin/class_main.jsp?market=2&issuetype=4';
+
+    // 取得 HTML 並轉換為 Big-5 編碼
+    const page = await firstValueFrom(this.httpService.get(url, { responseType: 'arraybuffer' }))
+      .then(response => iconv.decode(response.data, 'big5'));
+
+    // 使用 cheerio 載入 HTML 以取得表格的 table rows
+    const $ = cheerio.load(page);
+    const rows = $('.h4 tr');
+
+    // 遍歷每個 table row 並將其轉換成我們想要的資料格式
+    const data = rows.slice(1).map((i, el) => {
+      const td = $(el).find('td');
+      return {
+        symbol: td.eq(2).text().trim(),   // 股票代碼
+        name: td.eq(3).text().trim(),     // 股票名稱
+        market: td.eq(4).text().trim(),   // 市場別
+        industry: td.eq(6).text().trim(), // 產業別
+      };
+    }).toArray();
+
+    return data;
+  }
 }
