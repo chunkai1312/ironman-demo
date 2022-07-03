31e16457866d6b033e8e2f06db0819f108af12bd
Chun-Kai Wang <chunkai1312@gmail.com>
2022-07-03 16:20:12 +0800

    feat(scraper): fetch TWSE market trades

--- apps/scraper/src/app/scraper/twse-scraper.service.ts
+++ apps/scraper/src/app/scraper/twse-scraper.service.ts
@@ -1,5 +1,7 @@
+import * as numeral from 'numeral';
 import * as cheerio from 'cheerio';
 import * as iconv from 'iconv-lite';
+import { DateTime } from 'luxon';
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from 'rxjs';
@@ -54,6 +56,44 @@ export class TwseScraperService {
         industry: td.eq(6).text().trim(), // 產業別
       };
     }).toArray();
+
+    return data;
+  }
+
+  async fetchTwseMarketTrades(date: string) {
+    const query = new URLSearchParams({
+      response: 'json',                                   // 回傳格式為 JSON
+      date: DateTime.fromISO(date).toFormat('yyyyMMdd'),  // 將 ISO Date 格式轉換成 `yyyyMMdd`
+    });
+    const url = `https://www.twse.com.tw/exchangeReport/FMTQIK?${query}`;
+
+    // 取得回應資料
+    const responseData = await firstValueFrom(this.httpService.get(url))
+      .then(response => (response.data.stat === 'OK') && response.data);
+
+    // 若該日期非交易日或尚無成交資訊則回傳 null
+    if (!responseData) return null;
+
+    // 將回應資料整理成我們想要的資料格式
+    const data = responseData.data
+      .map(row => {
+        // 欄位依序為: 日期, 成交股數, 成交金額, 成交筆數, 發行量加權股價指數, 漲跌點數
+        const [ date, tradeVolume, tradeValue, transaction, price, change ] = row;
+
+        // 將 `民國年/MM/dd` 的日期格式轉換成 `yyyy-MM-dd`
+        const [ year, month, day ] = date.split('/');
+        const formattedDate = DateTime.fromFormat(`${+year + 1911}${month}${day}`, 'yyyyMMdd').toISODate();
+
+        return {
+          date: formattedDate,
+          tradeVolume: numeral(tradeVolume).value(),
+          tradeValue: numeral(tradeValue).value(),
+          transaction: numeral(transaction).value(),
+          price: numeral(price).value(),
+          change: numeral(change).value(),
+        };
+      })
+      .find(data => data.date === date) || null;  // 取得目標日期的成交資訊
 
     return data;
   }
